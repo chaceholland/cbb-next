8182306b5f2d0a9f3d068c71f3a096e95fe4e635
Chace <chace_holland@yahoo.com>
2026-02-18 10:10:09 -0600

    fix: disable Next.js image optimization globally to allow ESPN/sidearm CDN images

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'dtnozcqkuzhjmjvsfjqk.supabase.co' },
-      { protocol: 'https', hostname: 'a.espncdn.com' },
-      { protocol: 'https', hostname: 'images.sidearmdev.com', pathname: '/**' },
-      { protocol: 'https', hostname: '**.cloudfront.net' },
-      { protocol: 'https', hostname: 'd1baseball.com' },
-    ],
+    unoptimized: true,
   },
 };
 export default nextConfig;
